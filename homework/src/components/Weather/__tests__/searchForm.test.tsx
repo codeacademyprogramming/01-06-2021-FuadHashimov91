35189667aec574e863ed97dc06fb34ed0f47ff37
FuadHashimov91 <fuadbh@code.edu.az>
2021-06-02 02:46:23 +0400

    Comment render function

--- homework/src/components/Weather/__tests__/searchForm.test.tsx
+++ homework/src/components/Weather/__tests__/searchForm.test.tsx
@@ -3,22 +3,32 @@ import SearchWeather from '../SearchWeather';
 import ReactDOM from 'react-dom';
 import { fireEvent, getQueriesForElement } from '@testing-library/dom';
 
-const render = (component:any) => {
-    const root = document.createElement('div');
-    ReactDOM.render(component, root);
-    return getQueriesForElement(root);
-}
+// const render = (component:any) => {
+//     const root = document.createElement('div');
+//     ReactDOM.render(component, root);
+//     return getQueriesForElement(root);
+// }
 
 describe("Search Weather Component", () => {
     it('the snapshot', () => {
         const tree = renderer.create(<SearchWeather />).toJSON();
         expect(tree).toMatchSnapshot()
     });
-})
-test("renders the correct content", () => {
-    const { getByText, getByLabelText } = render(<SearchWeather/>);
 
-    const input = getByLabelText("Search");
-    fireEvent.change(input,{target :{ value : "Istanbul"}})
-    fireEvent.click(getByText('Search'));
+    // test("renders the correct content", () => {
+    //     // const { getByText, getByLabelText } = render(<SearchWeather/>);
+    
+    //     // const input = getByLabelText("Search");
+    //     // fireEvent.change(input,{target :{ value : "Istanbul"}})
+    //     // fireEvent.click(getByText('Search'));
+
+
+    // });
+
+})
+    
+
+it("renders correctly initial dom",()=>{
+    const input = document.querySelectorAll("input");
+    expect(input).toHaveLength(1);
 })
